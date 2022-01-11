1033b7ebe41324cb9b851a6d0b1d8f14c16b446a
alexsukhotckii <alex.al11212123@mmmaail.gmail>
2022-01-11 17:51:27 +0300

    12

--- arduino_code.ino
+++ arduino_code.ino
@@ -1,16 +1,15 @@
 int data;
 int LED = 13;
 void setup() {
-  Serial.begin(9600);                               //initialize serial COM at 9600 baudrate
-  pinMode(LED, OUTPUT);                    //declare the LED pin (13) as output
-  digitalWrite (LED, LOW);                     //Turn OFF the Led in the beginning
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  digitalWrite (LED, LOW);
 
-  Serial.println("Hello!,How are you Python ?");
 }
 String full_data = "", command = "";
 void loop() {
   
-  while (Serial.available())    //whatever the data that is coming in serially and assigning the value to the variable “data”
+  while (Serial.available())
   {
     data = Serial.read();
     if (data != 13)
@@ -22,8 +21,8 @@ void loop() {
      }
     
   }
-  if (command == "111110")
-    digitalWrite (LED, HIGH);                  //Turn On the Led
-  else if (command == "111102102")
-    digitalWrite (LED, LOW);                  //Turn OFF the Led
+  if (command == "on")
+    digitalWrite (LED, HIGH);
+  else if (command == "off")
+    digitalWrite (LED, LOW);
 }
