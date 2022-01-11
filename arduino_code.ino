1a353ba8be9f01f28e294ed03dbafbd55848a4b2
TemiusIII <artem.smirnov2005@mail.ru>
2022-01-11 22:13:54 +0300

    config start

---
+++ arduino_code.ino
@@ -0,0 +1,28 @@
+int data;
+int LED = 13;
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  digitalWrite (LED, LOW);
+
+}
+String full_data = "", command = "";
+void loop() {
+  
+  while (Serial.available())
+  {
+    data = Serial.read();
+    if (data != 13)
+      full_data += data;
+     else {
+      Serial.println(full_data);
+      command = full_data;
+      full_data = "";
+     }
+    
+  }
+  if (command == "on")
+    digitalWrite (LED, HIGH);
+  else if (command == "off")
+    digitalWrite (LED, LOW);
+}
---
+++ main.py
@@ -0,0 +1,33 @@
+import serial
+import speech_recognition as sr
+import json
+import serial.tools.list_ports
+import os
+
+debug = False
+if debug:
+    print(os.system('python3 -m serial.tools.list_ports'))
+    ArduinoUnoSerial = serial.Serial(input('Ur port: '), 9600)
+else:
+    ArduinoUnoSerial = serial.Serial('/dev/cu.usbmodem141401', 9600)
+r = sr.Recognizer()
+
+config = json.load('config.json')
+
+while 1:
+    with sr.Microphone(0) as source:
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio, language='ru-ru')
+            if debug:
+                print(text)
+            text = str(text).lower()
+
+            if text in comms.keys():
+                ArduinoUnoSerial.write(comms[text].encode('utf-8'))
+            else:
+                print('No such command')
+        except:
+            pass
+while 1:
+    inp = input()
