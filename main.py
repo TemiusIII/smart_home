cec4877cd05e4c667546065e41448192cd851f70
TemiusIII <artem.smirnov2005@mail.ru>
2022-01-11 21:55:07 +0300

    debug update v 0.2

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import serial
 import speech_recognition as sr
 import json
-import serial.tools.list_ports
-import os
 
 debug = False
-if debug:
-    print(os.system('python3 -m serial.tools.list_ports'))
-    ArduinoUnoSerial = serial.Serial(input('Ur port: '), 9600)
-else:
-    ArduinoUnoSerial = serial.Serial('/dev/cu.usbmodem141401', 9600)
+
+config = json.load('config')
+
+
+ArduinoUnoSerial = serial.Serial('/dev/cu.usbmodem141401', 9600)
+print('GGWP')
+
 r = sr.Recognizer()
-
-
 while 1:
     with sr.Microphone(0) as source:
         audio = r.listen(source)
